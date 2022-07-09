b2a6151d6dfbc39e4edba96cb2e67f12c95bb704
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-07-10 03:06:08 +0530

    Code

---
+++ Question-1.py
@@ -0,0 +1,19 @@
+#Algorithm to reverse the character array.
+
+from ast import main
+
+def reverse (array):
+    i = 0
+    j = len(array)-1
+    while i<j:
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        i+=1
+        j-=1
+        return array
+
+
+charArray = ['a','b','c','d']
+reverse(charArray)
+print(charArray)
